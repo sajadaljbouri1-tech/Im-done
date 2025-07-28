c42de8624f36dd569f03f51ac648c422ddbd2dea
sajadaljbouri1-tech <sajadaljbouri1@gmail.com>
2025-07-28 10:17:23 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -10,5 +10,5 @@ setInterval(() => {
   const minutes = String(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, '0');
   const seconds = String(Math.floor((distance % (1000 * 60)) / 1000)).padStart(2, '0');
 
-  countdown.innerHTML = \`\${days}D : \${hours}H : \${minutes}M : \${seconds}S\`;
+  countdown.innerHTML = `${days}D : ${hours}H : ${minutes}M : ${seconds}S`;
 }, 1000);
